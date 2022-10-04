e2cca9bb7ff1810ed898671f0809c06197c4d1ad
Saige Parrish <saige.parrish@gmail.com>
2022-10-04 17:06:56 -0400

    Add grid size manipulation to createGrid funciton

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-let grid = document.querySelector('.grid');
-grid.style.gridTemplateColumns = 'repeat(16, 1fr)' ;
-grid.style.gridTemplateRows = 'repeat(16, 1fr)' ;
-
-for(let i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('cell');
-    div.addEventListener('mouseenter', function(event){
-        event.target.style.backgroundColor = 'black';
-    })
-    grid.appendChild(div);
-};
 
 let reset = document.querySelector('.reset-btn');
 
 reset.addEventListener('click', function() {
     window.location.reload(true);
 })
+
+function createGrid(size) {
+
+    let grid = document.querySelector('.grid');
+    grid.style.gridTemplateColumns = `repeat(${size}, 2fr)` ;
+    grid.style.gridTemplateRows = `repeat(${size}, 2fr)` ;
+    let squares = grid.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+
+
+    let amount = size * size;
+    for(let i = 0; i < amount; i++) {
+        let div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseenter', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        grid.appendChild(div);
+    }
+};
+
+createGrid(24);
